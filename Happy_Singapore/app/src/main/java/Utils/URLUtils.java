646a17a2f63ffb0f5d20a0718a5069a2a386c706
WANG TIANYU <C200206@e.ntu.edu.sg>
2021-07-12 16:17:07 +0800

    Add English and Chinese name, icon v1.0.

--- Happy_Singapore/app/src/main/java/Utils/URLUtils.java
+++ Happy_Singapore/app/src/main/java/Utils/URLUtils.java
@@ -8,4 +8,6 @@ public class URLUtils {
     public static String weather = "http://www.weather.gov.sg/home/";
     //海峡时报
     public static String straits_times = "https://www.straitstimes.com/";
+    //新闻API
+    public static String news = "https://newsapi.org/v2/top-headlines?country=sg&apiKey=d69b7a41dbcb4ab29c842b66c9a893cc";
 }
---
+++ Happy_Singapore/app/src/main/java/common/test/TestActivity.java
@@ -0,0 +1,7 @@
+package common.test;
+
+import module.activity.BaseActivity;
+
+public class TestActivity extends BaseActivity {
+
+}
